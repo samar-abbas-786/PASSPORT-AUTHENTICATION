1ca19b432d85e6d35b1af5b0c996ca31cd67de19
Samar Abbas <samarabbas172003@gmail.com>
2024-06-28 15:07:06 +0530

    Passport Authentication4

--- config/auth.js
+++ config/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
   ensureAuthenticated: (req, res, next) => {
-    if (req.isAuthenticated) {
+    if (req.isAuthenticated()) {
       return next();
     } else {
       req.flash("error_msg", "You need to Loggin to access this resource");
